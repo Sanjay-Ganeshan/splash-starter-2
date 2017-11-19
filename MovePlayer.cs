bacd84eb4f6abd3761e3e8dea3788a6470138ef4
Sanjay Ganeshan <sangan@live.com>
2017-11-19 16:27:40 -0500

    Added movePlayer

---
+++ MovePlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+
+    public float movementSpeed = 4.0f;
+    public float angularSpeed = 120.0f;
+    public string s = "hello there";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+        this.gameObject.transform.Translate(verticalMovement * movementSpeed * Vector3.forward, Space.Self);
+        this.gameObject.transform.Rotate(Vector3.up, horizontalMovement * angularSpeed, Space.Self);
+
+
+    }
+}

10b752fccf5fbe7b4f2600e6402709ce98fca29b
Sanjay Ganeshan <sangan@live.com>
2017-11-19 17:52:13 -0500

    ispawn

---
+++ Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public float spawnTime = 4.0f;
+
+    public GameObject whatToSpawn;
+
+    private float timeSinceSpawn = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeSinceSpawn += Time.deltaTime;
+        if(timeSinceSpawn > spawnTime)
+        {
+            Instantiate(whatToSpawn, this.gameObject.transform);
+            timeSinceSpawn = 0;
+        }
+	}
+}
